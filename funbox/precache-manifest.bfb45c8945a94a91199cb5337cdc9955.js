15794af4ada36d4af2778615ec6e734f86707429
Artem <artskadin@gmail.com>
2020-12-15 21:18:22 +0300

    Add files via upload

---
+++ funbox/precache-manifest.bfb45c8945a94a91199cb5337cdc9955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce53e81f69e20bc1038f3c9d488f57b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ff9f085f3c456ce2d8",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aa6e7ad4bdf346005759",
+    "url": "/static/js/2.35f775a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.35f775a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ff9f085f3c456ce2d8",
+    "url": "/static/js/main.30ce4ef4.chunk.js"
+  },
+  {
+    "revision": "1f346cec00452bcdd10c",
+    "url": "/static/js/runtime-main.41187a14.js"
+  }
+]);
